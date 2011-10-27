05116c356f49cc67f1bacf9346c650f0000ff1a1
Taylor Bowland <taylor.bowland@gmail.com>
2011-10-27 00:04:23 -0700

    Last test

--- babynames.py
+++ babynames.py
@@ -14,21 +14,9 @@ import re
 Define the extract_names() function below and change main()
 to call it.
 
-For writing regex, it's nice to include a copy of the target
-text for inspiration.
-
-Here's what the html looks like in the baby.html files:
-...
-<h3 align="center">Popularity in 1990</h3>
-....
-<tr align="right"><td>1</td><td>Michael</td><td>Jessica</td>
-<tr align="right"><td>2</td><td>Christopher</td><td>Ashley</td>
-<tr align="right"><td>3</td><td>Matthew</td><td>Brittany</td>
-...
-
 Suggested milestones for incremental development:
- -Extract the year and print it
- -Extract the names and rank numbers and just print them
+ -Extract the year and print it DONE
+ -Extract the names and rank numbers and just print them DONE
  -Get the names data into a dict and print it
  -Build the [year, 'name rank', ... ] list and print it
  -Fix main() to use the extract_names list
