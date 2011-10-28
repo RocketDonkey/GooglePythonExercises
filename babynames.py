f1b757a2c95fee7bdbe2462293c592e9103de0a7
Taylor Bowland <taylor.bowland@gmail.com>
2011-10-27 20:32:01 -0700

    The REAL final code.

--- babynames.py
+++ babynames.py
@@ -17,9 +17,9 @@ to call it.
 Suggested milestones for incremental development:
  -Extract the year and print it DONE
  -Extract the names and rank numbers and just print them DONE
- -Get the names data into a dict and print it
- -Build the [year, 'name rank', ... ] list and print it
- -Fix main() to use the extract_names list
+ -Get the names data into a dict and print it DONE
+ -Build the [year, 'name rank', ... ] list and print it DONE
+ -Fix main() to use the extract_names list DONE
  -Practice posting this to GitHub! SWEET.
 """
 
@@ -35,14 +35,27 @@ def extract_names(filename):
 
   #Year
   year_regex = re.search(r'<h3 align=\"center\">Popularity\s*in\s*(\d+)</h3>', file_output)
+
+  if year_regex:year = year_regex.group(1)
+  else: year = 'No year found.'
   
   #Names and rank: rank (group 1), Male (group 2), Female (group 3)
-  re.findall(r'<tr align=\"right\"><td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>', file_output)
+  names_ranks = re.findall(r'<tr align=\"right\"><td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>', file_output)
+  
+  
+  #Create a dictionary of ranks and names
+  names_dict = {}
+  for items in names_ranks:
+    names_dict[items[2]] = items[0]
 
+  #Sort the dictionary and add to a list
+  names_list = [year] #Create the list and pre-populate with year
+  for key in sorted(names_dict.keys()):
+    names_list.append(key + ' ' + names_dict[key])
 
+  text = '\n'.join(names_list) + '\n'
 
-  #Print the year
-  print year_regex
+  print text
 
   return
 
@@ -51,8 +64,12 @@ def main():
   # This command-line parsing code is provided.
   # Make a list of command line arguments, omitting the [0] element
   # which is the script itself.
+  """
   args = sys.argv[1:]
+  #Don't have Python in the path, so I can't pass arguments (sweet)
+  #Changing it to accept a filename on input
 
+  args = None
   if not args:
     print 'usage: [--summaryfile] file [file ...]'
     sys.exit(1)
@@ -62,10 +79,15 @@ def main():
   if args[0] == '--summaryfile':
     summary = True
     del args[0]
+  """
 
-  # +++your code here+++
+  #Accept input (sys.argv doesn't work due to a path issue)
+  file_input = raw_input('Enter filename: ')
+  #file_input = 'baby1990.html'
+  
   # For each filename, get the names, then either print the text output
   # or write it to a summary file
+  extract_names(file_input)
   
 if __name__ == '__main__':
   main()
